e2a25ab3e7ea8d611ef70f7ec091340330a521cf
Aurora Dawn <131844170+StellarWitch7@users.noreply.github.com>
2024-06-10 00:21:49 -0400

    How silly

--- .bin/dev-moth-lang.sh
+++ .bin/dev-moth-lang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd ~/RiderProjects/moth-lang
-export NIXPKGS_ALLOW_UNFREE=1
-nix-shell -p vscode jetbrains.rider --run "code ~/VSCodeProjects/moth.code-workspace &
-  rider ./moth-lang.sln"
+nix-shell --run "export NIXPKGS_ALLOW_UNFREE=1
+nix-shell -p vscode jetbrains.rider --run \"code ~/VSCodeProjects/moth.code-workspace &
+rider ./moth-lang.sln\""
